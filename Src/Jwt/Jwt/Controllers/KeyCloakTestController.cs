c3499b2353d4f88e55ac0913338654215bd4d669
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2022-03-29 18:46:04 +0200

    Rename plus better api.

---
+++ Src/Jwt/Jwt/Controllers/KeyCloakTestController.cs
@@ -0,0 +1,34 @@
+﻿using Jwt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Jwt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KeyCloakTestController : ControllerBase
+    {
+        [HttpGet(Name = "Peek")]
+        [Route("Peek")]
+        public Object Peek()
+        {
+            return new
+            {
+                CurrentUser = GetCurrentUserInfo()
+            };
+        }
+
+        private object GetCurrentUserInfo()
+        {
+            if (ClaimsPrincipal.Current == null)
+            {
+                return UserInfo.Unauthenticated;
+            }
+
+            return new UserInfo()
+            {
+                IsAuthenticated = ClaimsPrincipal.Current.Identity.IsAuthenticated
+            };
+        }
+    }
+}
---
+++ Src/Jwt/Jwt/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt.Models
+{
+    public class UserInfo 
+    {
+        public static UserInfo Unauthenticated => new UserInfo();
+
+        public Boolean IsAuthenticated { get; set; }
+    }
+}
